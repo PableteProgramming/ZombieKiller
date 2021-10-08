1952b86b7bce7661e651ab173c977f60efefa3c7
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2021-10-08 20:49:32 +0200

    Adding color values

--- ZombieKiller/src/ZombieKiller.cpp
+++ ZombieKiller/src/ZombieKiller.cpp
@@ -15,7 +15,9 @@ int main()
     }
 
     int rowz = -1;
+    int cellz = -1;
     
+    //parsing row size
     if (root.isMember("rowz")) {
         if (root["rowz"].isInt()) {
             rowz = root["rowz"].asInt();
@@ -29,7 +31,23 @@ int main()
         std::cout << "rowz not found" << std::endl;
         return 1;
     }
-    std::cout << "rowz==" << rowz << std::endl;
+
+    //parsing cell size
+    if (root.isMember("cellz")) {
+        if (root["cellz"].isInt()) {
+            cellz = root["cellz"].asInt();
+        }
+        else {
+            std::cout << "cellz is not an int !" << std::endl;
+            return 1;
+        }
+    }
+    else {
+        std::cout << "cellz not found" << std::endl;
+        return 1;
+    }
+
+    //parsing map
     if (root.isMember("vmap")) {
         std::string vmap = root["vmap"].asString();
         int y = 0;
@@ -46,6 +64,61 @@ int main()
         std::cout << "vmap not found !" << std::endl;
         return 1;
     }
+
+    std::cout<<std::endl;
+
+    //parsing color values
+    std::vector <int> _0_colors;
+    std::vector <int> _1_colors;
+    std::vector <int> _2_colors;
+    if (root.isMember("colors")) {
+        if (root["colors"].isObject()) {
+            //parsing color0
+            if (root["colors"]["0"].isArray()) {
+                _0_colors.push_back(root["colors"]["0"][0].asInt());
+                _0_colors.push_back(root["colors"]["0"][1].asInt());
+                _0_colors.push_back(root["colors"]["0"][2].asInt());
+            }
+            else {
+                std::cout << "Colors 0 is not an array !" << std::endl;
+                return 1;
+            }
+
+            //parsing color1
+            if (root["colors"]["1"].isArray()) {
+                _1_colors.push_back(root["colors"]["1"][0].asInt());
+                _1_colors.push_back(root["colors"]["1"][1].asInt());
+                _1_colors.push_back(root["colors"]["1"][2].asInt());
+            }
+            else {
+                std::cout << "Colors 1 is not an array !" << std::endl;
+                return 1;
+            }
+
+            //parsing color2
+            if (root["colors"]["2"].isArray()) {
+                _2_colors.push_back(root["colors"]["2"][0].asInt());
+                _2_colors.push_back(root["colors"]["2"][1].asInt());
+                _2_colors.push_back(root["colors"]["2"][2].asInt());
+            }
+            else {
+                std::cout << "Colors 2 is not an array !" << std::endl;
+                return 1;
+            }
+        }
+        else {
+            std::cout << "colors is not an array !" << std::endl;
+            return 1;
+        }
+    }
+    else {
+        std::cout << "colors not found" << std::endl;
+        return 1;
+    }
+
+    std::cout << "color 0: " << _0_colors[0] << ";" << _0_colors[1] << ";" << _0_colors[2] << std::endl;
+    std::cout << "color 1: " << _1_colors[0] << ";" << _1_colors[1] << ";" << _1_colors[2] << std::endl;
+    std::cout << "color 2: " << _2_colors[0] << ";" << _2_colors[1] << ";" << _2_colors[2] << std::endl;
 
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
